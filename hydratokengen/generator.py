e6746484ebb5b7d6cd40b252ee9be7187f432987
Luka Zakrajšek <luka@bancek.net>
2021-10-21 12:06:14 +0200

    Add support for implicit flow and audience

--- hydratokengen/generator.py
+++ hydratokengen/generator.py
@@ -1,4 +1,7 @@
-from typing import Any, Dict, List
+import base64
+import json
+import uuid
+from typing import Any, Dict, List, Optional
 from urllib.parse import parse_qs, urlencode, urlparse, urlunparse
 
 import ory_hydra_client
@@ -20,10 +23,12 @@ class HydraTokenGen:
         hydra_public_url: str,
         hydra_admin_url: str,
         client_id: str,
-        client_secret: str,
         redirect_uri: str,
+        client_secret: Optional[str] = None,
         scope: str = "openid",
         token_endpoint_auth_method: str = "client_secret_basic",  # or "client_secret_post"
+        response_type: Optional[str] = None,
+        audience: Optional[str] = None,
     ):
         self._hydra_public_url = hydra_public_url
         self._hydra_admin_url = hydra_admin_url
@@ -32,6 +37,10 @@ class HydraTokenGen:
         self._redirect_uri = redirect_uri
         self._scope = scope
         self._token_endpoint_auth_method = token_endpoint_auth_method
+        self._response_type = (
+            response_type if response_type else ("code" if client_secret else "token")
+        )
+        self._audience = audience
 
         self._admin_configuration = ory_hydra_client.Configuration(host=hydra_admin_url)
         self._admin_client = ory_hydra_client.ApiClient(self._admin_configuration)
@@ -63,25 +72,27 @@ class HydraTokenGen:
                 consent_challenge, access_token, id_token, consent_request
             )
 
-            code = self._get_code(redirect)
-
-            token = self._exchange_code(code)
+            token = self._get_token(redirect)
 
             return token
         except Exception as exc:
             raise TokenGenerateException(f"Failed to generate Hydra token") from exc
 
     def _start_flow(self) -> str:
-        auth_code_url_query = urlencode(
-            {
-                "client_id": self._client_id,
-                "redirect_uri": self._redirect_uri,
-                "response_type": "code",
-                "scope": self._scope,
-                "state": "hydratokengen",
-            }
-        )
-        auth_code_url = f"{self._auth_url}?{auth_code_url_query}"
+        auth_code_url_query = {
+            "client_id": self._client_id,
+            "redirect_uri": self._redirect_uri,
+            "response_type": self._response_type,
+            "scope": self._scope,
+            "state": "hydratokengen",
+            "nonce": str(uuid.uuid4()),
+        }
+
+        if self._audience:
+            auth_code_url_query["audience"] = self._audience
+
+        auth_code_url_query_encoded = urlencode(auth_code_url_query)
+        auth_code_url = f"{self._auth_url}?{auth_code_url_query_encoded}"
 
         location_query = self._get_oauth2_flow_query(auth_code_url)
 
@@ -133,6 +144,14 @@ class HydraTokenGen:
 
         return api_response["redirect_to"]
 
+    def _get_token(self, auth_code_url: str) -> Token:
+        if self._response_type == "code":
+            code = self._get_code(auth_code_url)
+
+            return self._exchange_code(code)
+
+        return self._get_implicit_token(auth_code_url)
+
     def _get_code(self, auth_code_url: str) -> str:
         auth_code_url = urlunparse(
             self._hydra_public_url_parsed[:2] + urlparse(auth_code_url)[2:]
@@ -144,7 +163,7 @@ class HydraTokenGen:
 
         return code
 
-    def _exchange_code(self, code: str) -> Dict[str, Any]:
+    def _exchange_code(self, code: str) -> Token:
         data = {
             "grant_type": "authorization_code",
             "redirect_uri": self._redirect_uri,
@@ -168,6 +187,44 @@ class HydraTokenGen:
 
         return res.json()
 
+    def _get_implicit_token(self, auth_code_url: str) -> str:
+        auth_code_url = urlunparse(
+            self._hydra_public_url_parsed[:2] + urlparse(auth_code_url)[2:]
+        )
+
+        location_query = self._get_oauth2_flow_query(auth_code_url)
+
+        token = {}
+
+        for key, values in location_query.items():
+            if key == "state":
+                continue
+
+            token[key] = values[0]
+
+        if "expires_in" not in token and "id_token" in token:
+            # response_type=id_token does not add "expires_in" which we want to
+            # have in the token for caching. We can calculate it from "iat" and
+            # "exp" claims from the id_token
+            claims = self._get_id_token_claims_insecure(token["id_token"])
+
+            if claims and claims.get("iat") and claims.get("exp"):
+                token["expires_in"] = claims["exp"] - claims["iat"]
+
+        return token
+
+    @staticmethod
+    def _get_id_token_claims_insecure(id_token: str) -> Optional[Dict[str, Any]]:
+        if not id_token:
+            return None
+
+        parts = id_token.split(".")
+
+        if len(parts) != 3:
+            return None
+
+        return json.loads(base64.b64decode(parts[1]).decode("utf8"))
+
     def _get_oauth2_flow_query(self, auth_code_url: str) -> Dict[str, List[str]]:
         res = self._public_session.get(auth_code_url, allow_redirects=False)
 
@@ -180,7 +237,11 @@ class HydraTokenGen:
         if not location:
             raise KeyError("Missing Location header in response")
 
-        query = parse_qs(urlparse(location).query)
+        location_parsed = urlparse(location)
+
+        query = location_parsed.query or location_parsed.fragment
+
+        query = parse_qs(query)
 
         if query.get("error"):
             error = query["error"][0]
